bd7d399bacaab3970c86cedf22be5cc398a70a6b
Marques Johnson <marques.johnson1992@gmail.com>
2019-07-12 16:31:31 -0400

    will this go through

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,31 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(request => {
+    articleComponent(request);
+  }).catch(err => {
+    console.log(err);
+  });
+  
+  function articleComponent(promise) {
+    const cardsContainer = document.querySelector('.cards-container');
+    console.log(promise.data.articles);
+    for(const entry in promise.data.articles) {
+      const array = promise.data.articles[entry];
+      array.forEach(section => {
+        const card = document.createElement('div', 'card'),
+            headline = document.createElement('div', 'headline', section.headline),
+            author = document.createElement('div', 'author'),
+            img_container = document.createElement('div', 'img-container'),
+            img = document.createElement('img'),
+            by = document.createElement('span', `By ${section.authorName}`);
+        img_container.appendChild(img);
+        img.src = section.authorPhoto;
+        card.appendChild(headline);
+        card.appendChild(author);
+        author.appendChild(img_container);
+        author.appendChild(by);
+        cardsContainer.appendChild(card);
+      });
+    }
+  }
